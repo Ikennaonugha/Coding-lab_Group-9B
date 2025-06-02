08f9bca635b87a00d7486340c9884cc1abe7608b
Ikenna Onugha <i.onugha@alustudent.com>
2025-06-02 22:54:26 +0200

    Deleted analyze_logs.sh contents

--- analyze_logs.sh
+++ analyze_logs.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
 
-echo "Select log to archive:"
-echo "1) Heart Rate"
-echo "2) Temperature"
-echo "3) Water Usage"
-
-# prompt user to enter a choice
-read -p "Enter choice (1-3): " user_input
-
-if [[ $user_input != 1 || 2 || 3 ]] ; then
-	echo "Invalid input, enter 
